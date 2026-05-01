7a3eda6f435ed36e0b283a12ffe9e28d84929844
Mnigos <makowskiigor@gmail.com>
2026-05-01 15:35:38 +0200

    refactor(tasks): make task service async

--- src/tasks/application/task_service.rs
+++ src/tasks/application/task_service.rs
@@ -12,54 +12,43 @@ impl TaskService {
         Self { repository }
     }
 
-    pub fn list_all(&self) -> Result<Vec<Task>, TaskServiceError> {
+    pub async fn list_all(&self) -> Result<Vec<Task>, TaskServiceError> {
         let tasks = self
             .repository
-            .load()
+            .find_all()
+            .await
             .map_err(TaskServiceError::RepositoryFailed)?;
 
         Ok(tasks)
     }
 
-    pub fn add_task(&self, title: String) -> Result<Task, TaskServiceError> {
-        let mut tasks = self
-            .repository
-            .load()
-            .map_err(TaskServiceError::RepositoryFailed)?;
-
-        let next_id = tasks
-            .iter()
-            .map(|task| task.id().value())
-            .max()
-            .unwrap_or(0)
-            + 1;
-        let task_id = TaskId::new(next_id);
+    pub async fn add_task(&self, title: String) -> Result<Task, TaskServiceError> {
         let task_title = TaskTitle::new(title).map_err(TaskServiceError::InvalidTaskTitle)?;
-        let task = Task::new(task_id, task_title);
 
-        tasks.push(task.clone());
-
-        self.repository
-            .save(&tasks)
+        let task = self
+            .repository
+            .insert(&task_title)
+            .await
             .map_err(TaskServiceError::RepositoryFailed)?;
 
         Ok(task)
     }
 
-    pub fn complete_task(&self, id: &TaskId) -> Result<(), TaskServiceError> {
-        let mut tasks = self
+    pub async fn complete_task(&self, id: &TaskId) -> Result<(), TaskServiceError> {
+        let Some(mut task) = self
             .repository
-            .load()
-            .map_err(TaskServiceError::RepositoryFailed)?;
-
-        let Some(task) = tasks.iter_mut().find(|task| task.id() == id) else {
+            .find_by_id(id)
+            .await
+            .map_err(TaskServiceError::RepositoryFailed)?
+        else {
             return Err(TaskServiceError::TaskNotFound);
         };
 
         task.complete();
 
         self.repository
-            .save(&tasks)
+            .update(&task)
+            .await
             .map_err(TaskServiceError::RepositoryFailed)?;
 
         Ok(())
